cec130059a69a53b98ee97c0829aa3ec813e4614
Ida Utterström <idautterstrom@Idas-MacBook-Pro.local>
2023-07-12 22:06:33 +0200

    jdska

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 export const App = () => {
-  return <div>welcome</div>;
+  return (
+    <section>
+      <div>
+        <div>Image of me</div>
+        <div>Ida Utterström</div>
+      </div>
+      <div>
+        <div>Resume</div>
+        <div>Highlighted projects</div>
+        <div>hobbies</div>
+      </div>
+      <div>
+        <div>about me</div>
+      </div>
+      <div>
+        <div>testimonials</div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </section>
+  );
 };
