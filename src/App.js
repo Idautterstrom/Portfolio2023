c2fb640e823b023f853030daec8a78bc48dd05ec
Ida Utterström <idautterstrom@Idas-MacBook-Pro.local>
2023-07-12 22:35:52 +0200

    hero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 
+const HomeContainer = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const HeroImage = styled.div`
+  display: flex;
+  background-color: #ffa857;
+  height: 400px;
+  padding-top: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeroTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  background-color: #ff6b57;
+  padding-top: 40px;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const App = () => {
   return (
-    <section>
-      <div>
+    <HomeContainer>
+      <HeroImage>
         <div>Image of me</div>
+      </HeroImage>
+      <HeroTitle>
+        <div>logo</div>
         <div>Ida Utterström</div>
-      </div>
+        <div>Project manager</div>
+        <div>Button</div>
+      </HeroTitle>
       <div>
         <div>Resume</div>
         <div>Highlighted projects</div>
@@ -22,6 +51,6 @@ export const App = () => {
         <div>2</div>
         <div>3</div>
       </div>
-    </section>
+    </HomeContainer>
   );
 };
