516f19b723053ad8c7d4b6fc19fe91e9006b7fe8
Ida Utterström <idautterstrom@Idas-MacBook-Pro.local>
2023-07-13 12:06:49 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "../src/img/LOGO white.png";
+import Me from "../src/img/headerMe.png";
+import Clay from "../src/img/Clay.jpg";
+import Project from "../src/img/Project.jpg";
 
 const HomeContainer = styled.section`
   display: grid;
@@ -14,39 +18,133 @@ const HeroContainer = styled.div`
 const HeroImage = styled.div`
   display: flex;
   background-color: #ffa857;
-  height: 400px;
-  padding-top: 40px;
+  height: 450px;
   justify-content: center;
   align-items: center;
 `;
 
-const HeroTitle = styled.div`
+const MeContainer = styled.img`
+  display: flex;
+  height: 400px;
+`;
+
+const TitleContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
   background-color: #ff6b57;
-  padding-top: 40px;
   align-items: center;
   text-align: center;
   justify-content: center;
 `;
 
+const LogoContainer = styled.img`
+  display: flex;
+  height: 20px;
+`;
+
+const HeroName = styled.h1`
+  font-family: "syne", sans serif;
+  font-size: 25px;
+  color: white;
+`;
+
+const HeroSurname = styled.h1`
+  font-family: "syne", sans serif;
+  font-size: 25px;
+  color: white;
+  margin-top: -30px;
+`;
+
+const HeroTitle = styled.h2`
+  font-family: "Inter", sans-serif;
+  font-size: 15px;
+  padding-left: 100px;
+  padding-right: 100px;
+  margin-top: -20px;
+  color: white;
+`;
+
+const HeroButton = styled.button`
+  background-color: #ff99ff;
+  font-family: "syne", sans serif;
+  border: none;
+  padding: 10px 25px;
+  border-radius: 20px;
+`;
+
 const HighlightedContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  display: flex;
+  grid-gap: 20px;
+  padding: 40px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HighlightedImage = styled.img`
+  display: flex;
+  width: 300px;
+`;
+
+const SmallContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
   padding: 20px;
+  align-items: center;
+`;
+
+const HighlightedButton = styled.button`
+  font-family: "syne", sans serif;
+  border: 1px solid white;
+  color: white;
+  border-radius: 40px;
+  height: 40px;
+  width: 50px;
+  background-color: transparent;
+`;
+
+const ResumeContainer = styled.div`
+  width: 300px;
+  background-color: #ff6b57;
+`;
+
+const ProjectContainer = styled.div`
+  width: 300px;
+  background-color: #fdb3fd;
+`;
+
+const HobbiesContainer = styled.div`
+  width: 300px;
+  background-color: #ffa857;
 `;
 
 const AboutContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  padding: 20px;
+`;
+
+const BusinessContainer = styled.div`
+  background-color: #ff6b57;
+  padding: 40px;
+`;
+
+const AgilityContainer = styled.div`
+  background-color: #ffa857;
+  padding: 40px;
 `;
 
 const TestimonialsContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  padding: 20px;
+  grid-gap: 20px;
+  padding: 40px;
+`;
+
+const ContactContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  background-color: #fdb3fd;
+  padding: 40px;
 `;
 
 export const App = () => {
@@ -54,47 +152,56 @@ export const App = () => {
     <HomeContainer>
       <HeroContainer>
         <HeroImage>
-          <image>Image of me</image>
+          <MeContainer src={Me} alt="image of me" />
         </HeroImage>
-        <HeroTitle>
-          <image>logo</image>
-          <h1>Ida Utterström</h1>
-          <h2>
+        <TitleContainer>
+          <LogoContainer src={Logo} alt="logo" />
+          <HeroName>Ida</HeroName>
+          <HeroSurname>Utterström</HeroSurname>
+          <HeroTitle>
             Project manager, Business strategy, Marketing, People experience,
             Web development
-          </h2>
-          <button>Button</button>
-        </HeroTitle>
+          </HeroTitle>
+          <HeroButton>Contact me</HeroButton>
+        </TitleContainer>
       </HeroContainer>
       <HighlightedContainer>
-        <div>
-          <image>x</image>
-          <h2>Resume</h2>
-          <button>X</button>
-        </div>
-        <div>
-          <image>x</image>
-          <h2>Highlighted projects</h2>
-          <button>X</button>
-        </div>
-        <div>
-          <image>x</image>
-          <h2>Hobbies</h2>
-          <button>X</button>
-        </div>
+        <ResumeContainer>
+          <HighlightedImage src={Project} alt="Project" />
+          <SmallContainer>
+            <h2>Resume</h2>
+            <HighlightedButton>&rarr;</HighlightedButton>
+          </SmallContainer>
+        </ResumeContainer>
+        <ProjectContainer>
+          <HighlightedImage src={Project} alt="Project" />
+          <SmallContainer>
+            <h2>Projects</h2>
+            <HighlightedButton>&rarr;</HighlightedButton>
+          </SmallContainer>
+        </ProjectContainer>
+        <HobbiesContainer>
+          <HighlightedImage src={Clay} alt="clay" />
+          <SmallContainer>
+            <h2>Hobbies</h2>
+            <HighlightedButton>&rarr;</HighlightedButton>
+          </SmallContainer>
+        </HobbiesContainer>
       </HighlightedContainer>
       <AboutContainer>
-        <div>
+        <BusinessContainer>
           <image>x</image>
-          <h2>Business mindset</h2>
-          <p>
-            Throughout my career, I have consistently demonstrated strong
-            strategic thinking, creative visualization, and the ability to
-            execute tasks effectively. These attributes have enabled me to excel
-            in diverse environments and embrace new challenges.
-          </p>
-        </div>
-        <div>
+          <div>
+            <h2>Business mindset</h2>
+            <p>
+              Throughout my career, I have consistently demonstrated strong
+              strategic thinking, creative visualization, and the ability to
+              execute tasks effectively. These attributes have enabled me to
+              excel in diverse environments and embrace new challenges.
+            </p>
+          </div>
+        </BusinessContainer>
+        <AgilityContainer>
           <image>x</image>
           <h2>Agility</h2>
           <p>
@@ -110,9 +217,8 @@ export const App = () => {
               perspectives, uncovering opportunities for growth and improvement.
             </p>
           </div>
-        </div>
+        </AgilityContainer>
       </AboutContainer>
-      <h2>Things that have been said about me</h2>
       <TestimonialsContainer>
         <div>
           <image>x</image>
@@ -150,11 +256,16 @@ export const App = () => {
           </p>
         </div>
       </TestimonialsContainer>
-      <div>
-        <image>Logo</image>
-        <h1>Contact me</h1>
-        <button>contact</button>
-      </div>
+      <ContactContainer>
+        <div>
+          <LogoContainer src={Logo} alt="logo" />
+          <h1>Contact me</h1>
+          <button>contact</button>
+        </div>
+        <div>
+          <image>image</image>
+        </div>
+      </ContactContainer>
     </HomeContainer>
   );
 };
